79ce09e9caff39b33c75fb75ce1c61737470e6e5
Vilate <viknapp22@gmail.com>
2025-05-31 23:04:57 -0600

    fixed mongo connection from test

--- config/db.js
+++ config/db.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
+    console.log('MongoDB connected to:', conn.connection.name);
+    console.log('MongoDB host:', conn.connection.host);
   } catch (err) {
     console.error(err.message);
     process.exit(1);
