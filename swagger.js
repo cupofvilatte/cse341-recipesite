54b56a194f5edc31a0ee2bc2d360ef25728dd14d
Vilate <viknapp22@gmail.com>
2025-06-07 18:56:41 -0600

    fix/swagger api fixed remote render

--- swagger.js
+++ swagger.js
@@ -5,9 +5,13 @@ const doc = {
         title: 'Recipe API',
         description: 'API for managing recipes'
     },
+    host: 'cse341-recipesite.onrender.com',
+    schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log('Swagger doc generated');
+});
