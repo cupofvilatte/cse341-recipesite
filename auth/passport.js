a2def91cdc33b5d2e9c1d238cc1a54de4a808f3c
Vilate <viknapp22@gmail.com>
2025-06-16 14:35:47 -0600

    updated passport to reflect user creation for mongo

--- auth/passport.js
+++ auth/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
 
 passport.use(
     new GoogleStrategy(
@@ -9,21 +10,43 @@ passport.use(
         callbackURL: '/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
-      // You can connect this to a DB later to save or find users
-      // For now, just return the profile
-        return done(null, profile);
+        try {
+            // look for existing user
+            let existingUser = await User.findOne({ googleId: profile.id });
+
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+
+            // create new user if none exists
+            const newUser = await User.create({
+                googleId: profile.id,
+                displayName: profile.displayName,
+                email: profile.emails[0].value,
+                photo: profile.photos[0].value,
+            });
+
+            return done(null, newUser);
+        } catch (err) {
+            return done(err, null);
+        }
     }
     )
 );
 
 // Save user to session
 passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, user.id);
 });
 
 // Restore user from session
-passport.deserializeUser((user, done) => {
-    done(null, user);
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
 });
 
 module.exports = passport;
