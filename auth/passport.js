5ea7c38cd1aed2145b20f93655c927ed0b4618a2
Vilate <viknapp22@gmail.com>
2025-06-17 20:54:11 -0600

    updated passport callback URI

--- auth/passport.js
+++ auth/passport.js
@@ -7,7 +7,11 @@ passport.use(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL:
+            process.env.NODE_ENV === 'production'
+                ? 'https://cse341-recipesite.onrender.com/auth/google/callback'
+                : 'http://localhost:3000/auth/google/callback',
+
         prompt: 'select_account',
     },
     async (accessToken, refreshToken, profile, done) => {
